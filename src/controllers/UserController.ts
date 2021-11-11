6f64904c317bf1a0b8d10ce0bc1347daf1c87d1e
malolebrin <malolebrin@gmail.com>
2021-11-11 09:14:10 +0100

    FEAT add route to change user subscription

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,13 +1,13 @@
-import { paginator } from './../utils'
+import { paginator } from '@/utils'
 import { Request, Response } from "express"
 import { getManager } from "typeorm"
 import uid2 from "uid2"
-import Context from "../context"
-import { UserEntity, userSearchableFields } from "../entity/UserEntity"
-import checkUserRole from "../middlewares/checkUserRole"
-import { Role } from "../types/Role"
-import { generateHash, userResponse } from "../utils"
-import EventEntity from '../entity/EventEntity'
+import Context from "@/context"
+import { UserEntity, userSearchableFields } from "@/entity/UserEntity"
+import checkUserRole from "@/middlewares/checkUserRole"
+import { Role } from "@/types/Role"
+import { generateHash, userResponse } from "@/utils"
+import { SubscriptionEnum } from '@/types/Subscription'
 
 export default class UserController {
 
@@ -91,6 +91,21 @@ export default class UserController {
                 return userUpdated ? res.status(200).json(userResponse(userUpdated)) : res.status(400).json('user not updated')
             } else {
                 return res.status(401).json({ error: "unauthorized" })
+            }
+        } catch (error) {
+            return res.status(400).json({ error: error.message })
+        }
+    }
+
+    public static updatesubscription = async (req: Request, res: Response) => {
+        try {
+            const userId = parseInt(req.params.id)
+            const { subscription }: { subscription: SubscriptionEnum } = req.body
+            const user = await getManager().findOne(UserEntity, userId)
+            if (user) {
+                user.subscription = subscription
+                await getManager().save(user)
+                return res.status(200).json(userResponse(user))
             }
         } catch (error) {
             return res.status(400).json({ error: error.message })
