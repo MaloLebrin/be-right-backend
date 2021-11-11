e2fbc4f9b70646f4a261272c3e9f436019699f58
malolebrin <malolebrin@gmail.com>
2021-11-11 17:52:27 +0100

    FEAT can post and get files

--- src/controllers/FileController.ts
+++ src/controllers/FileController.ts
@@ -1,22 +1,20 @@
 import cloudinary from 'cloudinary'
-import { FileEntity } from "../entity/FileEntity"
+import { FileEntity, filesSearchableFields } from "../entity/FileEntity"
 import { Request, Response } from "express"
 import { getManager } from "typeorm"
+import { paginator } from '../utils'
 export default class FileController {
 
 	public static newFile = async (req: Request, res: Response) => {
 		try {
-			console.log(req.file, 'file')
 			const fileRecieved = req.file
 			const { name, description, user, event, employee }:
 				{ name: string, description: string, user?: number, event?: number, employee?: number } = req.body
 
-
-			console.log(req.body, 'req.body')
 			const result = await cloudinary.v2.uploader.upload(fileRecieved.path, {
 				folder: `beright/imageRight/user-${user}`
 			})
-			console.log(result, 'result')
+
 			const fileToPost = {
 				fileName: fileRecieved.filename,
 				name,
@@ -35,7 +33,6 @@ export default class FileController {
 				signature: result.signature,
 				mimeType: fileRecieved.mimetype,
 			}
-			console.log(fileToPost, 'fileToPost')
 
 			const file = getManager().create(FileEntity, fileToPost)
 			await getManager().save(file)
@@ -52,4 +49,101 @@ export default class FileController {
 			return res.status(400).json({ error: error.message })
 		}
 	}
+
+	public static getFile = async (req: Request, res: Response) => {
+		try {
+			const id = parseInt(req.params.id)
+			const file = await getManager().findOne(FileEntity, id)
+			if (file) {
+				return res.status(200).json(file)
+			} else {
+				return res.status(400).json({ message: 'File not found' })
+			}
+		} catch (error) {
+			return res.status(400).json({ error: error.message })
+		}
+	}
+
+	public static getFiles = async (req: Request, res: Response) => {
+		try {
+			const { ids }: { ids: number[] } = req.body
+			const files = await Promise.all(ids.map(id => getManager().findOne(FileEntity, id)))
+			if (files.length > 0) {
+				return res.status(200).json({ data: files, count: files.length })
+			} else {
+				return res.status(400).json({ message: 'Files not found' })
+			}
+		} catch (error) {
+			return res.status(400).json({ error: error.message })
+		}
+	}
+
+	public static getFilesByUser = async (req: Request, res: Response) => {
+		try {
+			const userId = parseInt(req.params.id)
+			const files = await getManager().find(FileEntity, { where: { user: userId } })
+			if (files.length > 0) {
+				return res.status(200).json({ data: files, count: files.length })
+			}
+			return res.status(400).json({ message: 'Files not found' })
+		} catch (error) {
+			return res.status(400).json({ error: error.message })
+		}
+	}
+
+	public static getFilesByEvent = async (req: Request, res: Response) => {
+		try {
+			const eventId = parseInt(req.params.id)
+			const files = await getManager().find(FileEntity, { where: { event: eventId } })
+			if (files.length > 0) {
+				return res.status(200).json({ data: files, count: files.length })
+			}
+			return res.status(400).json({ message: 'Files not found' })
+
+		} catch (error) {
+			return res.status(400).json({ error: error.message })
+		}
+	}
+
+	public static getFilesByEmployee = async (req: Request, res: Response) => {
+		try {
+			const employeeId = parseInt(req.params.id)
+			const files = await getManager().find(FileEntity, { where: { employee: employeeId } })
+			if (files.length > 0) {
+				return res.status(200).json({ data: files, count: files.length })
+			} else {
+				return res.status(400).json({ message: 'Files not found' })
+			}
+		} catch (error) {
+			return res.status(400).json({ error: error.message })
+		}
+	}
+
+	public static getFilesByUserAndEvent = async (req: Request, res: Response) => {
+		try {
+			const userId = parseInt(req.params.userId)
+			const eventId = parseInt(req.params.eventId)
+			const files = await getManager().find(FileEntity, { where: { user: userId, event: eventId } })
+			if (files.length > 0) {
+				return res.status(200).json({ data: files, count: files.length })
+			}
+			return res.status(400).json({ message: 'Files not found' })
+		} catch (error) {
+			return res.status(400).json({ error: error.message })
+		}
+	}
+
+	public static getAllPaginate = async (req: Request, res: Response) => {
+		try {
+			const queriesFilters = paginator(req, filesSearchableFields)
+			const [files, count] = await getManager().findAndCount(FileEntity, queriesFilters)
+			if (files.length > 0) {
+				return res.status(200).json({ data: files, total: count, currentPage: queriesFilters.page, limit: queriesFilters.take })
+			}
+			return res.status(400).json({ message: 'Files not found' })
+		} catch (error) {
+			return res.status(400).json({ error: error.message })
+		}
+	}
+	// TODO add update file and delte file
 }
--- src/routes/fileRoutes.ts
+++ src/routes/fileRoutes.ts
@@ -7,4 +7,15 @@ const router = Router()
 
 router.post('/', upload.single('file'), FileController.newFile)
 
+router.get('/', FileController.getAllPaginate)
+
+router.get('/:id', FileController.getFile)
+
+router.get('/many', FileController.getFiles)
+
+router.get('/user/:id', FileController.getFilesByUser)
+
+router.get('/event/:id', FileController.getFilesByEvent)
+
+
 export default router
