dae491448ba3b4e94840119bfb03d49843776f0e
malolebrin <malolebrin@gmail.com>
2022-01-12 18:25:52 +0100

    feat: init payment srvice

---
+++ src/services/PaymentService.ts
@@ -0,0 +1,8 @@
+import { PaymentEntity } from "../entity";
+
+export default class PaymentService {
+	constructor(payment: PaymentEntity) {
+		//todo function qui déteermine si isSuccess en fonction de !isFuture(Date.now(subimetedAT && exetuctedAt))))
+
+	}
+}
