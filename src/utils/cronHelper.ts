065db8bff89f09cf8919a4f4d86cd26048bd37a6
malolebrin <malolebrin@gmail.com>
2022-04-17 23:27:37 +0200

    feat: add possibility to do jobs

--- src/utils/cronHelper.ts
+++ src/utils/cronHelper.ts
@@ -4,5 +4,5 @@ export enum CronJobInterval {
   EVERY_30_MINUTES = '*/30 * * * *',
   EVERY_HOUR = '0 0 * * * *',
   EVERY_DAY_MIDNIGHT = '59 23 * * *',
-  EVERY_DAY_4_AM = '0 04 * * *',
+  EVERY_DAY_4_AM = '0 12 * * *',
 }
