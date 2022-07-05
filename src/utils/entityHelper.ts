416072dd6ea9de6adaf6d1552944425a95d418ae
malolebrin <malolebrin@gmail.com>
2022-07-05 10:24:17 +0200

    feat: entity helper can have array of number or array of entity

--- src/utils/entityHelper.ts
+++ src/utils/entityHelper.ts
@@ -1,9 +1,13 @@
 import { EntityRelationWithId } from "../types/Entities"
 import { isArrayOfNumbers } from "./arrayHelper"
 
-export function formatEntityRelationWithId(entities: EntityRelationWithId[]) {
-  if (entities && entities.length > 0 && !isArrayOfNumbers(entities)) {
-    return entities.map(entity => entity.id).filter(id => id)
+export function formatEntityRelationWithId(entities: EntityRelationWithId[]): number[] {
+  if (entities && entities.length > 0) {
+    if (!isArrayOfNumbers(entities)) {
+      return entities.map(entity => entity.id).filter(id => id)
+    } else {
+      return entities as unknown as number[]
+    }
   }
   return []
 }
