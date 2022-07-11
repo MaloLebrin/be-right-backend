b45745b96b615c0a141816fcc2d70c9150b4c8ba
MaloLebrin <malolebrin@gmail.com>
2022-07-11 17:59:51 +0200

    fix: cascade delete

--- src/entity/AddressEntitiy.ts
+++ src/entity/AddressEntitiy.ts
@@ -32,12 +32,12 @@ export class AddressEntity extends BaseEntity {
   @Column({ nullable: true })
   lng: number
 
-  @ManyToOne(() => UserEntity, user => user.address, { nullable: true })
+  @ManyToOne(() => UserEntity, user => user.address, { nullable: true, onDelete: 'CASCADE' })
   user: UserEntity | number
 
   @ManyToOne(() => EventEntity, event => event.address, { nullable: true, onDelete: 'CASCADE' })
   event: EventEntity | number
 
-  @ManyToOne(() => EmployeeEntity, employee => employee.address, { nullable: true })
+  @ManyToOne(() => EmployeeEntity, employee => employee.address, { nullable: true, onDelete: 'CASCADE' })
   employee: EmployeeEntity | number
 }
