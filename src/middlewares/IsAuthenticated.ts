acb1901e4e3af14d02611f43361ec3e25fce9e9f
MaloLebrin <malolebrin@gmail.com>
2022-10-03 10:42:06 +0200

    feat: add logs to middlewares

--- src/middlewares/IsAuthenticated.ts
+++ src/middlewares/IsAuthenticated.ts
@@ -3,8 +3,12 @@ import { getManager } from 'typeorm'
 import { isUserEntity } from '../utils/index'
 import Context from '../context'
 import { UserEntity } from '../entity/UserEntity'
+import { useLogger } from './loggerService'
 
 export default async function isAuthenticated(req: Request, res: Response, next: NextFunction) {
+  const { logger } = useLogger()
+  logger.info(`${req.url} check auth started`)
+
   if (req.headers.authorization) {
     const token = req.headers.authorization.replace('Bearer ', '')
 
@@ -12,11 +16,14 @@ export default async function isAuthenticated(req: Request, res: Response, next:
     if (user && isUserEntity(user)) {
       const ctx = Context.get(req)
       ctx.user = user
+      logger.info(`${req.url} User is allowed`)
       return next()
     } else {
+      logger.debug('user no allowed')
       return res.status(401).json({ error: 'unauthorized' })
     }
   } else {
+    logger.debug('user no allowed')
     return res.status(401).json({ error: 'unauthorized' })
   }
 }
