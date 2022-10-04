a495dbf1915ab0c52b34b87bf5e5177d1d74c3bb
MaloLebrin <malolebrin@gmail.com>
2022-10-04 13:53:11 +0200

    feat: create template for email to sign image right

--- src/services/MailService.ts
+++ src/services/MailService.ts
@@ -3,6 +3,8 @@ import nodemailer from 'nodemailer'
 import type { UserEntity } from '../entity'
 import { getfullUsername } from '../utils/userHelper'
 import { useEnv } from '../env'
+import type AnswerEntity from '../entity/AnswerEntity'
+import type EventEntity from '../entity/EventEntity'
 
 export default class MailService {
   public static async getConnection() {
@@ -40,6 +42,45 @@ export default class MailService {
             color: '#1b1e3dF2',
             text: 'Réinitialiser le mot de passe',
             link: `${FRONT_URL}/reset-password/${twoFactorRecoveryCode}/?email=${recipient.email}`,
+          },
+        },
+      },
+    }
+    // Generate an HTML email with the provided contents
+    const emailBody = mailGenerator.generate(email)
+
+    // Generate the plaintext version of the e-mail (for clients that do not support HTML)
+    const emailText = mailGenerator.generatePlaintext(email)
+    return { emailBody, emailText }
+  }
+
+  public static getSendImageRightTemplate({ recipient, event, answer }: { recipient: UserEntity; answer: AnswerEntity; event: EventEntity }) {
+    if (!recipient || !event || !answer) {
+      throw new Error('manque parametre pour envoyer l\'email')
+    }
+
+    const { FRONT_URL } = useEnv()
+    const mailGenerator = new Mailgen({
+      theme: 'default',
+      product: {
+        // Appears in header & footer of e-mails
+        name: 'BeRight',
+        link: 'https://mailgen.js/',
+        // Optional logo
+        // logo: 'https://mailgen.js/img/logo.png'
+      },
+    })
+
+    const email = {
+      body: {
+        name: `${getfullUsername(recipient)}`,
+        intro: 'Vous recevez ce mail car vous avez une demande d\'utilisation de votre image à remplur.',
+        action: {
+          instructions: 'Clickez sur le bouton ci-dessous pour voir le document.',
+          button: {
+            color: '#1b1e3dF2',
+            text: 'Voir le document',
+            link: `${FRONT_URL}/answer/${answer.id}`,
           },
         },
       },
