7c5a7bd1d5a414d4bd218ebc649e8060a5946cf2
malolebrin <malolebrin@gmail.com>
2022-12-20 17:02:35 +0100

    🌱 create Admin data seeders

--- src/seed/UserCompany/index.ts
+++ src/seed/UserCompany/index.ts
@@ -1,15 +1,42 @@
 import UserService from '../../services/UserService'
 import { AddressService } from '../../services/AddressService'
+import EmployeeService from '../../services/EmployeeService'
+import EventService from '../../services/EventService'
+import AnswerService from '../../services/AnswerService'
+import type EventEntity from '../../entity/EventEntity'
 import {
   addressFixtureCompanyPremium,
+  employeesFixtureCompanyPremium,
+  eventFixtureCompanyPremium,
   userCompanyFixturePremium,
 } from './fixtures'
 
 export async function seedUserCompany() {
   const user = await UserService.createOneUser(userCompanyFixturePremium)
 
-  const userAddress = await AddressService.createOne({
+  await AddressService.createOne({
     address: addressFixtureCompanyPremium,
     userId: user.id,
   })
+
+  const employees = await Promise.all(employeesFixtureCompanyPremium.map(
+    async item => await EmployeeService.createOne(item.employee, user.id),
+  ))
+
+  const employeeIds = employees.map(employee => employee.id)
+
+  const event = await EventService.createOneEvent(
+    {
+      ...eventFixtureCompanyPremium,
+      employeeIds,
+      createdByUser: user.id,
+    } as unknown as Partial<EventEntity>,
+    user.id,
+    1,
+  )
+
+  console.log(event, '<==== event')
+
+  const answers = await AnswerService.createMany(event.id, employeeIds)
+  console.log(answers, '<==== answers')
 }
--- src/seed/index.ts
+++ src/seed/index.ts
@@ -1,7 +1,9 @@
+import { createAdminUser } from './admin'
 import { createPhotographers } from './shared/photographerFixtures'
+import { seedUserCompany } from './UserCompany'
 
 async function createDevSeeders() {
   await createPhotographers()
+  await createAdminUser()
+  await seedUserCompany()
 }
-
-// createDevSeeders()
