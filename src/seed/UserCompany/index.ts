0d4b5908d6ebf001c8d205bde73b5a09c3189c55
malolebrin <malolebrin@gmail.com>
2022-12-21 00:10:43 +0100

    🌱 add seeder for another user and his employees

--- src/seed/UserCompany/index.ts
+++ src/seed/UserCompany/index.ts
@@ -1,3 +1,4 @@
+import { getManager } from 'typeorm'
 import UserService from '../../services/UserService'
 import { AddressService } from '../../services/AddressService'
 import EmployeeService from '../../services/EmployeeService'
@@ -5,9 +6,13 @@ import EventService from '../../services/EventService'
 import AnswerService from '../../services/AnswerService'
 import type EventEntity from '../../entity/EventEntity'
 import {
+  addressFixtureCompanyMedium,
   addressFixtureCompanyPremium,
+  employeesFixtureCompanyMedium,
   employeesFixtureCompanyPremium,
+  eventFixtureCompanyMedium,
   eventFixtureCompanyPremium,
+  userCompanyFixtureMedium,
   userCompanyFixturePremium,
 } from './fixtures'
 
@@ -41,4 +46,48 @@ export async function seedUserCompany() {
   })
 
   await AnswerService.createMany(event.id, employeeIds)
+  const answer = await AnswerService.getOneAnswerForEventEmployee(event.id, employeeIds[0])
+  if (answer) {
+    answer.hasSigned = true
+    answer.signedAt = new Date()
+    await getManager().save(answer)
+  }
+}
+
+export async function seedMediumUserData() {
+  const user = await UserService.createOneUser(userCompanyFixtureMedium)
+
+  await AddressService.createOne({
+    address: addressFixtureCompanyMedium,
+    userId: user.id,
+  })
+
+  const employees = await Promise.all(employeesFixtureCompanyMedium.map(
+    async item => await EmployeeService.createOne(item.employee, user.id),
+  ))
+
+  const employeeIds = employees.map(employee => employee.id)
+
+  const event = await EventService.createOneEvent(
+    {
+      ...eventFixtureCompanyMedium.event,
+      employeeIds,
+      createdByUser: user.id,
+    } as unknown as Partial<EventEntity>,
+    user.id,
+    1,
+  )
+
+  await AddressService.createOne({
+    address: eventFixtureCompanyMedium.address,
+    eventId: event.id,
+  })
+
+  await AnswerService.createMany(event.id, employeeIds)
+  const answer = await AnswerService.getOneAnswerForEventEmployee(event.id, employeeIds[0])
+  if (answer) {
+    answer.hasSigned = true
+    answer.signedAt = new Date()
+    await getManager().save(answer)
+  }
 }
