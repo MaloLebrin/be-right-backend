c7592cea8f0998e440f40ac6d0abd9b11b6b5eba
malolebrin <malolebrin@gmail.com>
2022-12-28 10:14:07 +0100

    🚀 Add scripts to deploy test DB with seeders 🌱

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,16 @@
 #!/app/bin/env bash
 
+set -eo pipefail
+
 echo PORT $PORT
 echo ENV $NODE_ENV
+
+if [ $NODE_ENV = 'test' ];
+then
+  exec node /app/build/src/seed/index.js &
+  node /app/build/src/index.js
+
+else  
 exec node /app/build/src/index.js
-# & node /app/build/src/jobs.js
+fi
 
--- scriptSeed.sh
+++ scriptSeed.sh
@@ -1,3 +1,5 @@
 #!/app/bin/env bash
 
 exec node /app/build/src/seed/index.js
+
+exit 1
