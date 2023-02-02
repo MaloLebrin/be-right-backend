dd48588abdcd348b04fe02174fb3203a298037b4
malolebrin <malolebrin@gmail.com>
2023-02-02 12:55:27 +0100

    ♻️ Refactoring use logger instead of console

--- src/jobs/updateEventsStatusJob.ts
+++ src/jobs/updateEventsStatusJob.ts
@@ -1,17 +1,20 @@
 import dayjs from 'dayjs'
 import type { DataSource } from 'typeorm'
 import EventEntity from '../entity/EventEntity'
+import { useLogger } from '../middlewares/loggerService'
 import EventService from '../services/EventService'
 
 export default async function udpateEventStatusJob(APP_SOURCE: DataSource) {
+  const { logger } = useLogger()
+
   try {
     const dateStart = dayjs().locale('fr').format('YYYY-MM-DD-HH-mm')
-    console.warn(`Sarting update event status at ${dateStart}`)
+    logger.warn(`Sarting update event status at ${dateStart}`)
 
     const eventService = new EventService(APP_SOURCE)
 
     const events = await APP_SOURCE.manager.find(EventEntity)
-    console.info(events.length, 'events')
+    logger.info(events.length, 'events')
 
     if (events.length > 0) {
       await Promise.all(events.map(event => eventService.getNumberSignatureNeededForEvent(event.id)))
@@ -19,9 +22,9 @@ export default async function udpateEventStatusJob(APP_SOURCE: DataSource) {
       await eventService.updateStatusForEventArray(events)
     }
   } catch (error) {
-    console.error(error, 'error')
+    logger.error(error, 'error')
   } finally {
     const dateEnd = dayjs().locale('fr').format('YYYY-MM-DD-HH-mm')
-    console.warn(`update event status ended at ${dateEnd}`)
+    logger.warn(`update event status ended at ${dateEnd}`)
   }
 }
