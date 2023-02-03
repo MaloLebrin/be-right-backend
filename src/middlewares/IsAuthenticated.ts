295bc0a918736f80e32ef2658d939a1d71264bea
malolebrin <malolebrin@gmail.com>
2023-02-03 12:07:26 +0100

    🔐 Add JWt verifications

--- src/middlewares/IsAuthenticated.ts
+++ src/middlewares/IsAuthenticated.ts
@@ -1,16 +1,23 @@
 import type { NextFunction, Request, Response } from 'express'
+import { verify } from 'jsonwebtoken'
 import { isUserEntity } from '../utils/index'
 import Context from '../context'
 import { UserEntity } from '../entity/UserEntity'
 import { APP_SOURCE, REDIS_CACHE } from '..'
+import { useEnv } from '../env'
 import { useLogger } from './loggerService'
 
 export default async function isAuthenticated(req: Request, res: Response, next: NextFunction) {
   const { logger } = useLogger()
+  const { JWT_SECRET } = useEnv()
   logger.info(`${req.url} check auth started`)
 
   if (req.headers.authorization) {
     const token = req.headers.authorization.replace('Bearer ', '')
+
+    const verifyToken = verify(token, JWT_SECRET)
+
+    logger.debug(verifyToken)
 
     const user = await REDIS_CACHE.get<UserEntity>(
       `user-token-${token}`,
