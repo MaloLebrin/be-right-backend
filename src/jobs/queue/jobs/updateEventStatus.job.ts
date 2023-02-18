0a02609c557e39f9730c44d9f7f25cf08d0d6111
malolebrin <malolebrin@gmail.com>
2023-02-18 11:59:47 +0100

    🧑‍💻 Update type inference queue job

--- src/jobs/queue/jobs/updateEventStatus.job.ts
+++ src/jobs/queue/jobs/updateEventStatus.job.ts
@@ -6,14 +6,14 @@ import type { JobImp } from './job.definition'
 import { BaseJob } from './job.definition'
 
 export class UpdateEventStatusJob extends BaseJob implements JobImp {
-  constructor(public payoad: Record<string, unknown>) {
+  constructor(public payoad: Record<string, number>) {
     super()
   }
 
   handle = async () => {
     const eventService = new EventService(APP_SOURCE)
     const { eventId } = this.payoad
-    const event = await eventService.getOneEvent(eventId as number)
+    const event = await eventService.getOneEvent(eventId)
 
     if (event) {
       logger.info(event, 'event')
