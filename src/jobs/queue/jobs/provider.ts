c6370b070d9998f0c4a4aa24689d3aa731c4ba9c
malolebrin <malolebrin@gmail.com>
2023-02-18 18:50:15 +0100

    ✨ Add send mail queue job

--- src/jobs/queue/jobs/provider.ts
+++ src/jobs/queue/jobs/provider.ts
@@ -1,9 +1,11 @@
 import { plainToInstance } from 'class-transformer'
 import type { JobImp } from './job.definition'
+import { SendMailAnswerCreationjob } from './sendMailAnswerCreation.job'
 import { UpdateEventStatusJob } from './updateEventStatus.job'
 
 export const JobDictonary = new Map([
   [UpdateEventStatusJob.name, UpdateEventStatusJob],
+  [SendMailAnswerCreationjob.name, SendMailAnswerCreationjob],
 ])
 
 export const getJobInstance = (data: JobImp): JobImp => {
