790a4003183621bdee8e894476962e440c798b0a
malolebrin <malolebrin@gmail.com>
2023-02-24 15:29:34 +0100

    🔐more strong checkerrors

--- src/utils/answerHelper.ts
+++ src/utils/answerHelper.ts
@@ -1,7 +1,7 @@
 import type AnswerEntity from '../entity/AnswerEntity'
 
 export function isAnswerSigned(answer: AnswerEntity): boolean {
-  return answer.signedAt !== null
+  return answer.signedAt !== null && answer.signedAt !== undefined
 }
 
 export function isAnswerTruthy(answer: AnswerEntity): boolean {
