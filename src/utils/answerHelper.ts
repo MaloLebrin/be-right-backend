5ca7e21f26d0abb62d1e1c20b95a8b4218aa598a
malolebrin <malolebrin@gmail.com>
2023-03-23 12:45:28 +0100

    🔐 Add function to filter answer's keys

--- src/utils/answerHelper.ts
+++ src/utils/answerHelper.ts
@@ -7,3 +7,20 @@ export function isAnswerSigned(answer: AnswerEntity): boolean {
 export function isAnswerTruthy(answer: AnswerEntity): boolean {
   return answer.hasSigned
 }
+
+export const secretAnswerKeys = [
+  'twoFactorSecret',
+  'twoFactorCode',
+  'token',
+]
+
+export function answerResponse(answer: AnswerEntity) {
+  const entityReturned = {}
+  for (const [key, value] of Object.entries(answer)) {
+    if (!secretAnswerKeys.includes(key)) {
+      // eslint-disable-next-line security/detect-object-injection
+      entityReturned[key] = value
+    }
+  }
+  return entityReturned as AnswerEntity
+}
