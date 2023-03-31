ce20cb2fd32fd9573958fdd35811746d6f5ba8de
Malo <malolebrin@gmail.com>
2023-03-31 16:31:34 +0200

    🚀Config redis db for prod (#175)
    * ✨Add warning when prod mode
    
    * 🚀 Deployement worked
    
    * 🚀 Un comment cron start

--- ormconfig.ts
+++ ormconfig.ts
@@ -4,20 +4,10 @@ export const dataBaseConfig = {
   production: {
     name: 'production',
     type: 'postgres',
-    host: 'postgres',
-    port: 5432,
-    username: process.env.DB_USERNAME_PROD,
-    password: process.env.DB_PASSWORD_PROD,
-    database: 'be-right-db',
     url: process.env.DATABASE_URL,
     synchronize: true,
     logging: false,
     ssl: true,
-    extra: {
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    },
     entities: [
       ...entities,
     ],
@@ -67,7 +57,7 @@ export const dataBaseConfig = {
     port: process.env.POSTGRES_PORT_TEST,
     username: process.env.DB_USERNAME_TEST,
     password: process.env.DB_PASSWORD_TEST,
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL_TEST,
     database: 'be-right-backend-db',
     synchronize: true,
     logging: false,
