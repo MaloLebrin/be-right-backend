063170f38d4d0af32191c16404e6d6d5f85946d1
malolebrin <malolebrin@gmail.com>
2023-05-03 19:13:05 +0200

    ♻️ clean db calls

--- src/controllers/DownloadController.ts
+++ src/controllers/DownloadController.ts
@@ -2,22 +2,26 @@ import { unlink } from 'node:fs'
 import type { Request, Response } from 'express'
 import puppeteer from 'puppeteer'
 import type { Logger } from 'pino'
+import type { Repository } from 'typeorm'
+import { In } from 'typeorm'
 import { APP_SOURCE } from '..'
 import { ApiError } from '../middlewares/ApiError'
-// import type RedisCache from '../RedisCache'
 import AnswerService from '../services/AnswerService'
 import EventService from '../services/EventService'
 import { hasOwnProperty } from '../utils/objectHelper'
 import { wrapperRequest } from '../utils'
+import { parseQueryIds } from '../utils/basicHelper'
 import UserService from '../services/UserService'
 import { AddressService } from '../services'
 import type { EmployeeEntity } from '../entity/employees/EmployeeEntity'
 import { logger } from '../middlewares/loggerService'
 import { CompanyService } from '../services/CompanyService'
 import { isUserOwner } from '../utils/userHelper'
+import AnswerEntity from '../entity/AnswerEntity'
 
 export default class AuthController {
   AnswerService: AnswerService
+  repository: Repository<AnswerEntity>
   AddressService: AddressService
   EventService: EventService
   UserService: UserService
@@ -32,6 +36,7 @@ export default class AuthController {
   }>
 
   constructor() {
+    this.repository = APP_SOURCE.getRepository(AnswerEntity)
     this.AnswerService = new AnswerService(APP_SOURCE)
     this.AddressService = new AddressService(APP_SOURCE)
     this.UserService = new UserService(APP_SOURCE)
@@ -48,15 +53,27 @@ export default class AuthController {
       const id = parseInt(req.params.id)
 
       if (id) {
-        const answer = await this.AnswerService.getOne(id, true)
+        const answer = await this.repository.findOne({
+          where: {
+            id,
+          },
+          relations: [
+            'event.company',
+            'event.company.address',
+            'event.company.users',
+            'event.partner',
+            'employee',
+            'employee.address',
+          ],
+        })
 
         if (answer && hasOwnProperty(answer, 'event') && hasOwnProperty(answer, 'employee')) {
           const event = answer.event
           const employee = answer.employee as EmployeeEntity
 
-          const partner = await this.UserService.getOne(event.partnerId)
-          const company = await this.CompanyService.getOne(event.companyId, true)
-          const employeeAddress = await this.AddressService.getOne(employee.addressId)
+          const partner = event.partner
+          const company = event.company
+          const employeeAddress = answer.employee.address
           const user = company.users.find(user => isUserOwner(user))
 
           return res.render('answer', {
@@ -87,71 +104,85 @@ export default class AuthController {
     await wrapperRequest(req, res, async () => {
       const id = parseInt(req.params.id)
 
-      if (id) {
-        const answer = await this.AnswerService.getOne(id, true)
+      if (!id) {
+        throw new ApiError(422, 'L\'identifiant de la réponse est requis')
+      }
 
-        const employee = answer?.employee as EmployeeEntity
-        if (employee) {
-          const baseUrl = `${req.protocol}://${req.get('host')}`
-          const url = `${baseUrl}/answer/view/${id}`
-          console.log(url, '<==== url')
-          // const url = `${baseUrl}/orders/${id}/view`
+      const answer = await this.AnswerService.getOne(id, true)
 
-          const filePath = `/app/uploads/ANSWER-${employee.slug}.pdf`
+      const employee = answer?.employee as EmployeeEntity
+      if (employee) {
+        const baseUrl = `${req.protocol}://${req.get('host')}`
+        const url = `${baseUrl}/answer/view/${id}`
 
-          try {
-            const browser = await puppeteer.launch({
-              headless: true,
-              executablePath: '/usr/bin/chromium-browser',
-              args: [
-                '--no-sandbox',
-                '--disable-gpu',
-              ],
-            })
+        const filePath = `/app/uploads/droit-image-${employee.slug}.pdf`
 
-            const page = await browser.newPage()
+        try {
+          const browser = await puppeteer.launch({
+            headless: true,
+            executablePath: '/usr/bin/chromium-browser',
+            args: [
+              '--no-sandbox',
+              '--disable-gpu',
+            ],
+          })
 
-            await page.goto(url)
-            const pdf = await page.pdf({ path: filePath, format: 'a4', printBackground: true })
-            await browser.close()
+          const page = await browser.newPage()
 
-            return res
-              .set({ 'Content-Type': 'application/pdf', 'Content-Length': pdf.length })
-              .download(filePath)
-          } catch (error) {
-            this.logger.error(error)
-            return res.status(error.status || 500).send({
-              success: false,
-              message: error.message,
-              stack: error.stack,
-              description: error.cause,
-            })
-          } finally {
-            await this.delay(1000)
+          await page.goto(url)
+          const pdf = await page.pdf({ path: filePath, format: 'a4', printBackground: true })
+          await browser.close()
 
-            // eslint-disable-next-line security/detect-non-literal-fs-filename
-            unlink(filePath, err => {
-              if (err) {
-                this.logger.error(err)
-                return res.status(422).send({
-                  success: false,
-                  message: err.message,
-                  stack: err.stack,
-                  description: err.cause,
-                })
-              } else {
-                this.logger.info(`${filePath} was deleted`)
-              }
-            })
-          }
+          return res
+            .set({ 'Content-Type': 'application/pdf', 'Content-Length': pdf.length })
+            .download(filePath)
+        } catch (error) {
+          this.logger.error(error)
+          return res.status(error.status || 500).send({
+            success: false,
+            message: error.message,
+            stack: error.stack,
+            description: error.cause,
+          })
+        } finally {
+          await this.delay(1000)
+
+          // eslint-disable-next-line security/detect-non-literal-fs-filename
+          unlink(filePath, err => {
+            if (err) {
+              this.logger.error(err)
+              return res.status(422).send({
+                success: false,
+                message: err.message,
+                stack: err.stack,
+                description: err.cause,
+              })
+            } else {
+              this.logger.info(`${filePath} was deleted`)
+            }
+          })
         }
       }
-      // throw new ApiError(422, 'L\'identifiant de la réponse est requis').Handler(res)
     })
   }
 
-  public getPDFAnswer = async (req: Request, res: Response) => {
+  public downLoadAnswers = async (req: Request, res: Response) => {
     await wrapperRequest(req, res, async () => {
+      const answerIds = parseQueryIds(req.query.ids as string)
+
+      if (!answerIds || answerIds.length < 1) {
+        throw new ApiError(422, 'L\'identifiant de la réponse est requis')
+      }
+
+      const answers = await this.repository.find({
+        where: {
+          id: In(answerIds),
+        },
+        relations: {
+          employee: true,
+        },
+      })
+      return answers
     })
   }
 }
