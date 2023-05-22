89ea8b9a5dad45d0dc540d0d3cce1fa9921650e6
Malo <malolebrin@gmail.com>
2023-05-22 12:46:09 +0200

    🩹fix-invalidate-user-in-cache-when-creating-group (#239)
    fix-invalidate-user-in-cache-when-creating-group

--- src/jobs/crons/sendMailBeforeStartEvent.cron.ts
+++ src/jobs/crons/sendMailBeforeStartEvent.cron.ts
@@ -46,6 +46,8 @@ export async function sendMailBeforeStartEvent(APP_SOURCE: DataSource) {
           answer,
         })),
       )
+
+      logger.info(`${answers.length} Relances ont été envoyées`)
     }
   } catch (error) {
     logger.error(error, 'error')
