054ed06412b888e731652c0f68b20477f1ce64b6
malolebrin <malolebrin@gmail.com>
2023-06-03 16:23:44 +0200

    ✨ Admin group routes

--- src/routes/Admin/AdminGroupRoutes.ts
+++ src/routes/Admin/AdminGroupRoutes.ts
@@ -4,6 +4,7 @@ import { Role } from '../../types'
 import type { BaseInterfaceRouter } from '../BaseRouter'
 import { BaseRouter } from '../BaseRouter'
 import { GroupController } from '../../controllers/employees/GroupController'
+import { AdminGroupController } from '../../controllers/Admin/employees/GroupController'
 
 const {
   idParamsSchema,
@@ -16,7 +17,8 @@ export class AdminGroupRoutes extends BaseRouter implements BaseInterfaceRouter 
   public intializeRoutes = () => {
     this.router.get('/', [isAuthenticated, checkUserRole(Role.ADMIN)], new GroupController(this.DATA_SOURCE).getAll)
     this.router.patch('/:id', [validate(idParamsSchema), isAuthenticated, checkUserRole(Role.ADMIN)], new GroupController(this.DATA_SOURCE).updateOne)
-    // this.router.post('/:id', [validate(idParamsSchema), isAuthenticated, checkUserRole(Role.ADMIN)], new GroupController(this.DATA_SOURCE).updateOne)
+    this.router.post('/', [validate(idParamsSchema), isAuthenticated, checkUserRole(Role.ADMIN)], new AdminGroupController(this.DATA_SOURCE).createOne)
+    this.router.post('/csv', [validate(idParamsSchema), isAuthenticated, checkUserRole(Role.ADMIN)], new AdminGroupController(this.DATA_SOURCE).createOneWithCSV)
 
     return this.router
   }
