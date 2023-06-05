75048735eaa70b3efce1510aa455fe21645c2edd
Malo <malolebrin@gmail.com>
2023-06-05 17:58:11 +0200

    🚑 Fix path event routes (#266)

--- src/routes/EventRoutes.ts
+++ src/routes/EventRoutes.ts
@@ -15,15 +15,15 @@ export class EventRoutes extends BaseRouter implements BaseInterfaceRouter {
   constructor(SOURCE: DataSource) { super(SOURCE) }
 
   public intializeRoutes = () => {
-    this.router.get('/event/manyByIds', [isAuthenticated], new EventController(this.DATA_SOURCE).getMany)
-    this.router.get('/event/', [isAuthenticated], new EventController(this.DATA_SOURCE).getAll)
-    this.router.get('/event/user', [isAuthenticated], new EventController(this.DATA_SOURCE).getAllForUser)
-    this.router.get('/event/deleted', [isAuthenticated], new EventController(this.DATA_SOURCE).getAllDeletedForUser)
-    this.router.get('/event/withRelations/:id', [validate(idParamsSchema), isAuthenticated], new EventSpecificController(this.DATA_SOURCE).fetchOneEventWithRelations)
-    this.router.get('/event/:id', [validate(idParamsSchema), isAuthenticated], new EventController(this.DATA_SOURCE).getOne)
-    this.router.post('/event', [validate(createOneEventSchema), isAuthenticated], new EventSpecificController(this.DATA_SOURCE).posteOneWithRelations)
-    this.router.patch('/event/:id', [validate(idParamsSchema), isAuthenticated], new EventController(this.DATA_SOURCE).updateOne)
-    this.router.delete('/event/:id', [validate(idParamsSchema), isAuthenticated], new EventController(this.DATA_SOURCE).deleteOne)
+    this.router.get('/manyByIds', [isAuthenticated], new EventController(this.DATA_SOURCE).getMany)
+    this.router.get('/', [isAuthenticated], new EventController(this.DATA_SOURCE).getAll)
+    this.router.get('/user', [isAuthenticated], new EventController(this.DATA_SOURCE).getAllForUser)
+    this.router.get('/deleted', [isAuthenticated], new EventController(this.DATA_SOURCE).getAllDeletedForUser)
+    this.router.get('/withRelations/:id', [validate(idParamsSchema), isAuthenticated], new EventSpecificController(this.DATA_SOURCE).fetchOneEventWithRelations)
+    this.router.get('/:id', [validate(idParamsSchema), isAuthenticated], new EventController(this.DATA_SOURCE).getOne)
+    this.router.post('', [validate(createOneEventSchema), isAuthenticated], new EventSpecificController(this.DATA_SOURCE).posteOneWithRelations)
+    this.router.patch('/:id', [validate(idParamsSchema), isAuthenticated], new EventController(this.DATA_SOURCE).updateOne)
+    this.router.delete('/:id', [validate(idParamsSchema), isAuthenticated], new EventController(this.DATA_SOURCE).deleteOne)
 
     return this.router
   }
