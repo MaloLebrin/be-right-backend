346e5c656cdcae12ba76e8fa3dca36dce7ff3506
malolebrin <malolebrin@gmail.com>
2023-06-06 14:21:56 +0200

    🩹 Eslint errors

--- src/controllers/FileController.ts
+++ src/controllers/FileController.ts
@@ -30,7 +30,7 @@ export default class FileController {
     await wrapperRequest(req, res, async () => {
       const fileRecieved = req.file
       const { name, description, event, employee, type }:
-        { name: string; description: string; event?: number; employee?: number; type: FileTypeEnum } = req.body
+      { name: string; description: string; event?: number; employee?: number; type: FileTypeEnum } = req.body
 
       const ctx = Context.get(req)
 
