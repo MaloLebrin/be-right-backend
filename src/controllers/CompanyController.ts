029afd447ebaf1d9457d729847c542bef8b43701
malolebrin <malolebrin@gmail.com>
2023-06-07 16:44:43 +0200

    🔐 Add more security of changing role of user

--- src/controllers/CompanyController.ts
+++ src/controllers/CompanyController.ts
@@ -8,7 +8,7 @@ import { ApiError } from '../middlewares/ApiError'
 import Context from '../context'
 import { APP_SOURCE } from '..'
 import { Role } from '../types'
-import { isUserOwner } from '../utils/userHelper'
+import { isUserAdmin, isUserOwner } from '../utils/userHelper'
 
 export class CompanyController {
   CompanyService: CompanyService
@@ -43,50 +43,50 @@ export class CompanyController {
 
       const companyId = ctx.user.companyId
 
-      if (id && companyId) {
-        const company = await this.repository.findOne({
-          where: { id: companyId },
-          relations: {
-            users: true,
-          },
-        })
-
-        if (!company) {
-          throw new ApiError(422, 'L\'entreprise n\'éxiste pas')
-        }
-
-        const user = await this.UserRepository.findOne({
-          where: {
-            id,
-            company: {
-              id: company.id,
-            },
-          },
-        })
-
-        if (!user) {
-          throw new ApiError(422, 'L\'utilisateur n\'éxiste pas')
-        }
-
-        if (!company.userIds.includes(user.id)) {
-          throw new ApiError(422, 'L\'utilisateur ne vous appartient pas')
-        }
-
-        if (isUserOwner(user)) {
-          user.roles = Role.USER
-        } else {
-          user.roles = Role.OWNER
-        }
-
-        const userToSend = await this.UserRepository.save(user)
-
-        return res.status(200).json({
-          user: userToSend,
-          company,
-        })
+      if (!id || !companyId) {
+        throw new ApiError(422, 'L\'identifiant de l\'utilisateur est requis')
       }
 
-      throw new ApiError(422, 'L\'identifiant de l\'utilisateur est requis')
+      const company = await this.repository.findOne({
+        where: { id: companyId },
+        relations: {
+          users: true,
+        },
+      })
+
+      if (!company) {
+        throw new ApiError(422, 'L\'entreprise n\'éxiste pas')
+      }
+
+      const user = await this.UserRepository.findOne({
+        where: {
+          id,
+          company: {
+            id: company.id,
+          },
+        },
+      })
+
+      if (!user) {
+        throw new ApiError(422, 'L\'utilisateur n\'éxiste pas')
+      }
+
+      if (isUserAdmin(user)) {
+        throw new ApiError(401, 'Vous ne pouvez pas changer le rôle de cet utilisateur')
+      }
+
+      if (!company.userIds.includes(user.id)) {
+        throw new ApiError(422, 'L\'utilisateur ne vous appartient pas')
+      }
+
+      user.roles = isUserOwner(user) ? Role.USER : Role.OWNER
+
+      const userToSend = await this.UserRepository.save(user)
+
+      return res.status(200).json({
+        user: userToSend,
+        company,
+      })
     })
   }
 
