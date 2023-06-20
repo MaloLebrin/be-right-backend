22d7a9f3ed8ffa81254626cf8e963d88a79b8023
malolebrin <malolebrin@gmail.com>
2023-06-20 23:04:18 +0200

    ✨ Route get amount of product

--- src/controllers/stripe/StripeProductController.ts
+++ src/controllers/stripe/StripeProductController.ts
@@ -1,6 +1,9 @@
 import type { NextFunction, Request, Response } from 'express'
+import type Stripe from 'stripe'
 import { StripeService } from '../../services/StripeService'
 import { wrapperRequest } from '../../utils'
+import { ApiError } from '../../middlewares/ApiError'
+import { fromCent } from '../../utils/paymentHelper'
 
 export class StripeProductController extends StripeService {
   constructor() { super() }
@@ -9,6 +12,26 @@ export class StripeProductController extends StripeService {
     await wrapperRequest(req, res, next, async () => {
       const products = await this.stripe.products.list({ expand: ['data.default_price'] })
       return res.status(200).json(products.data)
+    })
+  }
+
+  public getAmountForProduct = async (req: Request, res: Response, next: NextFunction) => {
+    await wrapperRequest(req, res, next, async () => {
+      const { productId, quantity }: { productId: string; quantity: number } = req.body
+
+      if (!productId) {
+        throw new ApiError(422, 'Identifiant du produit manquand')
+      }
+
+      const product = await this.stripe.products.retrieve(productId, { expand: ['default_price'] })
+      const price = product.default_price as Stripe.Price
+
+      return res.status(200).json({
+        id: productId,
+        unitAmout: fromCent(price.unit_amount),
+        totalAmount: fromCent(price.unit_amount * quantity),
+        quantity,
+      })
     })
   }
 }
--- src/routes/PaymentRoutes.ts
+++ src/routes/PaymentRoutes.ts
@@ -1,13 +1,19 @@
 import type { DataSource } from 'typeorm'
 import { StripeProductController } from '../controllers/stripe/StripeProductController'
+import { useValidation } from '../middlewares'
+import { productIdParamsValidation } from '../middlewares/validation/stripe/productValidation'
 import type { BaseInterfaceRouter } from './BaseRouter'
 import { BaseRouter } from './BaseRouter'
+
+const { validate } = useValidation()
 
 export class PaymentsRoutes extends BaseRouter implements BaseInterfaceRouter {
   constructor(SOURCE: DataSource) { super(SOURCE) }
 
   public intializeRoutes = () => {
     this.router.get('/products', new StripeProductController().getProducts)
+    this.router.post('/productAmount', [validate(productIdParamsValidation)], new StripeProductController().getAmountForProduct)
+
     return this.router
   }
 }
