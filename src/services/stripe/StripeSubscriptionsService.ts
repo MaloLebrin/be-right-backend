2165b7c01b4ceb97b2660f754a3083053d447841
malolebrin <malolebrin@gmail.com>
2023-06-28 16:25:04 +0200

    ✨ update api subscripiton with strip subscriptions

--- src/services/stripe/StripeSubscriptionsService.ts
+++ src/services/stripe/StripeSubscriptionsService.ts
@@ -1,10 +1,17 @@
+import type { DataSource } from 'typeorm'
+import dayjs from 'dayjs'
 import type { CancelReasonEnum, StripeProductForSession } from '../../types/Stripe/Index'
 import { StripeCurrency } from '../../types/Stripe/Payment'
+import { SubscriptionService } from '../SubscriptionService'
+import type { SubscriptionEnum } from '../../types'
 import { StripeService } from './StripeService'
 
 export class StripeSubscriptionService extends StripeService {
-  constructor() {
+  private SubscriptionService: SubscriptionService
+
+  constructor(APP_SOURCE: DataSource) {
     super()
+    this.SubscriptionService = new SubscriptionService(APP_SOURCE)
   }
 
   public getAllStripeSubscriptions = async () => {
@@ -12,28 +19,43 @@ export class StripeSubscriptionService extends StripeService {
   }
 
   public createStripeSubscription = async ({
+    subscriptionId,
     stripeCustomerId,
     products,
   }: {
+    subscriptionId: number
     stripeCustomerId: string
     products: StripeProductForSession[]
   }) => {
-    return this.stripe.subscriptions.create({
+    const stripeSubscription = await this.stripe.subscriptions.create({
       customer: stripeCustomerId,
       currency: StripeCurrency.EUR,
       items: products,
     })
+
+    await this.SubscriptionService.updateOne(subscriptionId, {
+      expireAt: dayjs(stripeSubscription.canceled_at).toDate(),
+      type: stripeSubscription.metadata.apiType as SubscriptionEnum,
+    })
+
+    return stripeSubscription
   }
 
   public cancelStripeSubscription = async ({
+    subscriptionId,
     stripeSubscriptionId,
     reason,
     comment,
   }: {
+    subscriptionId: number
     stripeSubscriptionId: string
     reason?: CancelReasonEnum
     comment?: string
   }) => {
+    await this.SubscriptionService.updateOne(subscriptionId, {
+      expireAt: dayjs().toDate(),
+    })
+
     return this.stripe.subscriptions.cancel(stripeSubscriptionId, {
       cancellation_details: {
         feedback: reason,
