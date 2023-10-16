f243d3d655c425f412c820be540fd964049533ef
Malo <malolebrin@gmail.com>
2023-10-16 09:18:54 +0200

    ✨Auth logout request (#349)
    * 🐛 Fix bug on redis Cache
    
    * ✨ Add request to logout and clear cache
    
    * 🩹Review changes

--- src/routes/AuthRoutes.ts
+++ src/routes/AuthRoutes.ts
@@ -1,5 +1,6 @@
 import type { DataSource } from 'typeorm'
 import {
+  isAuthenticated,
   useValidation,
 } from '../middlewares'
 import AuthController from '../controllers/AuthController'
@@ -20,6 +21,7 @@ export class AuthRoutes extends BaseRouter implements BaseInterfaceRouter {
     this.router.post('/forgot-password', [validate(emailAlreadyExistSchema)], new AuthController(this.DATA_SOURCE).forgotPassword)
     this.router.post('/reset-password', [validate(resetPasswordSchema)], new AuthController(this.DATA_SOURCE).resetPassword)
     this.router.post('/signup', [validate(registerSchema)], new AuthController(this.DATA_SOURCE).signUp)
+    this.router.post('/logout', [isAuthenticated], new AuthController(this.DATA_SOURCE).logOut)
 
     return this.router
   }
