71a184f8ce20f08a94e4b4c2d6696a90560771f1
MaloLebrin <malolebrin@gmail.com>
2023-10-20 12:33:30 +0200

    ✅ Test company helper

---
+++ src/tests/utils/companyHelper.test.ts
@@ -0,0 +1,22 @@
+import { expect, test } from 'vitest'
+import companyJSON from '../fixtures/premium/company.json'
+import {
+  isBasicCie,
+  isMediumCie,
+  isPremiumCie,
+} from '../../utils/companyHelper'
+import type { CompanyEntity } from '../../entity/Company.entity'
+
+const company = companyJSON as unknown as CompanyEntity
+
+test('isBasicCie, isMediumCie, isPremiumCie, send right bool', () => {
+  expect(isPremiumCie(company)).toBeTruthy()
+  expect(isMediumCie(company)).toBeFalsy()
+  expect(isBasicCie(company)).toBeFalsy()
+  expect(isPremiumCie(null as unknown as CompanyEntity)).toBeFalsy()
+  expect(isMediumCie(null as unknown as CompanyEntity)).toBeFalsy()
+  expect(isBasicCie(null as unknown as CompanyEntity)).toBeFalsy()
+  expect(isPremiumCie(undefined as unknown as CompanyEntity)).toBeFalsy()
+  expect(isMediumCie(undefined as unknown as CompanyEntity)).toBeFalsy()
+  expect(isBasicCie(undefined as unknown as CompanyEntity)).toBeFalsy()
+})
