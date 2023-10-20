aaf0a0c716a10546ab1073dd930dba20e0fc80ab
Malo <malolebrin@gmail.com>
2023-10-20 12:35:21 +0200

    ✅ Test company helper (#361)

--- src/utils/companyHelper.ts
+++ src/utils/companyHelper.ts
@@ -2,13 +2,13 @@ import type { CompanyEntity } from '../entity/Company.entity'
 import { SubscriptionEnum } from '../types'
 
 export function isPremiumCie(company: CompanyEntity) {
-  return company.subscriptionLabel === SubscriptionEnum.PREMIUM
+  return company?.subscriptionLabel === SubscriptionEnum.PREMIUM
 }
 
 export function isMediumCie(company: CompanyEntity) {
-  return company.subscriptionLabel === SubscriptionEnum.MEDIUM
+  return company?.subscriptionLabel === SubscriptionEnum.MEDIUM
 }
 
 export function isBasicCie(company: CompanyEntity) {
-  return company.subscriptionLabel === SubscriptionEnum.BASIC
+  return company?.subscriptionLabel === SubscriptionEnum.BASIC
 }
