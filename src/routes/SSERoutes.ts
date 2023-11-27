b701d1a59a74f64fbd18030eac8ba62ef13315ad
Malo <malolebrin@gmail.com>
2023-11-27 01:15:49 +0100

    🚑 Add wrapper function for request handling in (#411)
    SSERoutes

--- src/routes/SSERoutes.ts
+++ src/routes/SSERoutes.ts
@@ -1,8 +1,10 @@
+import type { NextFunction, Request, Response } from 'express'
 import type { DataSource } from 'typeorm'
 import { type Channel, createSession } from 'better-sse'
 import { verify } from 'jsonwebtoken'
 import { useEnv } from '../env'
 import { NotificationSSEService } from '../services/notifications/notificationSSEService'
+import { wrapperRequest } from '../utils'
 import type { BaseInterfaceRouter } from './BaseRouter'
 import { BaseRouter } from './BaseRouter'
 
@@ -19,28 +21,30 @@ export class SSERoutes extends BaseRouter implements BaseInterfaceRouter {
   }
 
   public intializeRoutes = () => {
-    this.router.get('/:token', async (req, res) => {
-      const token = req.params.token
+    this.router.get('/:token', async (req: Request, res: Response, next: NextFunction) => {
+      await wrapperRequest(req, res, next, async () => {
+        const token = req.params.token
 
-      const { JWT_SECRET } = useEnv()
-      verify(token, JWT_SECRET)
+        const { JWT_SECRET } = useEnv()
+        verify(token, JWT_SECRET)
 
-      if (token) {
-        const session = await createSession(req, res)
-        this.notificationChannel.register(session)
-        const notificationSSEService = new NotificationSSEService(this.DATA_SOURCE)
+        if (token) {
+          const session = await createSession(req, res)
+          this.notificationChannel.register(session)
+          const notificationSSEService = new NotificationSSEService(this.DATA_SOURCE)
 
-        setInterval(async () => {
-          const notifications = await notificationSSEService.getAllUser({
-            notificationToken: token,
-          })
+          setInterval(async () => {
+            const notifications = await notificationSSEService.getAllUser({
+              notificationToken: token,
+            })
 
-          if (notifications?.length > 0) {
-            this.notificationChannel.broadcast(notifications, 'message')
-          }
-          // 5 secondes
-        }, 5000)
-      }
+            if (notifications?.length > 0) {
+              this.notificationChannel.broadcast(notifications, 'message')
+            }
+            // 5 secondes
+          }, 5000)
+        }
+      })
     })
 
     return this.router
