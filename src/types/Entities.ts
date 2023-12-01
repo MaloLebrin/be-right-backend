4bce372a4027c59526e6d47818d5b31ea0b1211a
MaloLebrin <malolebrin@gmail.com>
2023-12-01 11:40:01 +0100

    Remove BugReportEntity import from Entities.ts

--- src/types/Entities.ts
+++ src/types/Entities.ts
@@ -1,8 +1,7 @@
 import type { UserEntity } from '../entity/UserEntity'
 import type EventEntity from '../entity/EventEntity'
-import type { BugReportEntity } from '../entity/BugReportEntity'
 import type { EmployeeEntity } from '../entity/employees/EmployeeEntity'
 import type { FileEntity } from '../entity/FileEntity'
 
 export type EntityRelationWithId =
-  EventEntity | UserEntity | FileEntity | EmployeeEntity | BugReportEntity
+  EventEntity | UserEntity | FileEntity | EmployeeEntity
