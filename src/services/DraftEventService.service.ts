2419b6aa23d729285497dee90f6a6000c4ae5188
MaloLebrin <malolebrin@gmail.com>
2024-02-28 11:09:18 +0100

    ✨ Add DraftEventService class with create, get, and delete methods

---
+++ src/services/DraftEventService.service.ts
@@ -0,0 +1,60 @@
+import type { DataSource, Repository } from 'typeorm'
+import { DraftEventEntity } from '../entity/event/DraftEvent.entity'
+import type { EventWithRelationsCreationPayload } from '../types/Event'
+
+export class DraftEventService {
+  private repository: Repository<DraftEventEntity>
+
+  constructor(APP_SOURCE: DataSource) {
+    this.repository = APP_SOURCE.getRepository(DraftEventEntity)
+  }
+
+  private composeDraftEvent = ({
+    companyId,
+    checkoutSessionId,
+    data,
+  }: {
+    companyId: number
+    checkoutSessionId: string
+    data: EventWithRelationsCreationPayload
+  }) => {
+    const { event, address, photographerId } = data
+    return {
+      companyId,
+      checkoutSessionId,
+      addressData: address,
+      eventData: { ...event, photographerId },
+    }
+  }
+
+  public async createDraftEvent({
+    companyId,
+    checkoutSessionId,
+    data,
+  }: {
+    companyId: number
+    checkoutSessionId: string
+    data: EventWithRelationsCreationPayload
+  }) {
+    const draftEvent = this.repository.create(this.composeDraftEvent({
+      companyId,
+      checkoutSessionId,
+      data,
+    }))
+    return this.repository.save(draftEvent)
+  }
+
+  public async getDraftEventByCheckoutSessionId(checkoutSessionId: string) {
+    return this.repository.findOne({
+      where: {
+        checkoutSessionId,
+      },
+    })
+  }
+
+  public async deleteDraftEventByCheckoutSessionId(checkoutSessionId: string) {
+    return this.repository.delete({
+      checkoutSessionId,
+    })
+  }
+}
