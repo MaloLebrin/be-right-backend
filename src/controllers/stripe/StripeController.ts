e67b6d7ca5879738d4e69f47c3bac5aaff86c2cf
MaloLebrin <malolebrin@gmail.com>
2024-02-29 13:20:01 +0100

    ✨ Create route to create stripe checkout session and draftEvent pending on payment

--- src/controllers/stripe/StripeController.ts
+++ src/controllers/stripe/StripeController.ts
@@ -4,16 +4,19 @@ import { StripeCustomerService } from '../../services/stripe/stripeCustomer.serv
 import { wrapperRequest } from '../../utils'
 import { ApiError } from '../../middlewares/ApiError'
 import { StripeCheckoutSessionService } from '../../services/stripe/stripeCheckout.service'
-import { ModePaymentEnum } from '../../types/Stripe'
+import { ModePaymentEnum, type StripeCheckoutSessionCreationPayload } from '../../types/Stripe'
+import { DraftEventService } from '../../services/DraftEventService.service'
 
 export class StripeController {
   private StripeCustomerService: StripeCustomerService
   private StripeCheckoutSessionService: StripeCheckoutSessionService
+  private DraftEventService: DraftEventService
 
   constructor(DATA_SOURCE: DataSource) {
     if (DATA_SOURCE) {
       this.StripeCustomerService = new StripeCustomerService(DATA_SOURCE)
       this.StripeCheckoutSessionService = new StripeCheckoutSessionService()
+      this.DraftEventService = new DraftEventService(DATA_SOURCE)
     }
   }
 
@@ -26,23 +29,47 @@ export class StripeController {
       const user = ctx.user
       const stripeCustomer = await this.StripeCustomerService.getStripeCustomerForUser(user)
 
-      if (!stripeCustomer) {
-        throw new ApiError(500, 'Une erreur s\'est produite')
+      if (!stripeCustomer || !user.email || !user.companyId) {
+        throw new ApiError(500, 'Une erreur s\'est produite impossible d\'identifier l\'utilisateur')
       }
 
-      const { nbRecipients, priceId }: { nbRecipients: number; priceId: string } = req.body
+      const {
+        priceId,
+        event,
+        address,
+        photographerId,
+      }: StripeCheckoutSessionCreationPayload = req.body
 
-      await this.StripeCheckoutSessionService.createStripeCheckoutSession({
+      const session = await this.StripeCheckoutSessionService.createStripeCheckoutSession({
         stripeCustomerId: stripeCustomer.id,
-        customerEmail: user.email,
         modePayment: ModePaymentEnum.PAYMENT,
         products: [
           {
             price: priceId,
-            quantity: nbRecipients,
+            quantity: event.employeeIds?.length,
           },
         ],
       })
+
+      if (!session) {
+        throw new ApiError(500, 'Une erreur s\'est produite lors de la création de la session de paiement')
+      }
+
+      const draftEvent = await this.DraftEventService.createDraftEvent({
+        checkoutSessionId: session.id,
+        companyId: user.companyId,
+        data: {
+          event,
+          address,
+          photographerId,
+        },
+      })
+
+      if (!draftEvent) {
+        throw new ApiError(500, 'Une erreur s\'est produite')
+      }
+
+      return res.status(200).json({ sessionUrl: session.url })
     })
   }
 }
---
+++ src/middlewares/validation/stripe/payEvent.ts
@@ -0,0 +1,29 @@
+import {
+  array,
+  date,
+  number,
+  object,
+  string,
+} from 'yup'
+
+export const payEventValidation = object({
+  body: object({
+    priceId: string().required('L\'identifiant du prix est requis'),
+    address: object({
+      addressLine: string().required('L\'adresse est requise'),
+      addressLine2: string().nullable(),
+      postalCode: string().required('Le code postal est requis'),
+      city: string().required('La ville est requise'),
+      country: string().required('Le pays est requis'),
+    }),
+    event: object({
+      name: string().required('le nom de l\'événement est obligatoire'),
+      description: string().nullable(),
+      start: date().required('La date de début est obligatoire'),
+      end: date().required('La date de fin est obligatoire'),
+      employeeIds: array().of(number()).min(1, 'Sélectionnez au moins un destinataire')
+        .required('Les destinataires sont obligatoire'),
+    }),
+    photographerId: number().required('L\'identifiant du photographe est requis'),
+  }),
+})
