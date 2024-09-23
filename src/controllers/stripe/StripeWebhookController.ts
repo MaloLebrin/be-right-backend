938de2fe87e983993be5f16fddaa130c2f00066a
MaloLebrin <malolebrin@gmail.com>
2024-09-23 10:07:08 +0200

    Refactor StripeWebhookController to handle different checkout session events

--- src/controllers/stripe/StripeWebhookController.ts
+++ src/controllers/stripe/StripeWebhookController.ts
@@ -2,7 +2,7 @@ import type { DataSource } from 'typeorm'
 import type { NextFunction, Request, Response } from 'express'
 import { wrapperRequest } from '../../utils'
 import { StripeWebhookService } from '../../services/stripe/stripeWebhook.service'
-import type { WebhookEvent } from '../../types/Stripe'
+import type { StripeWebhookEventObject, WebhookEvent } from '../../types/Stripe'
 import { logger } from '../../middlewares/loggerService'
 
 export class StripeWebhookController {
@@ -19,18 +19,22 @@ export class StripeWebhookController {
       // const sig = req.headers['stripe-signature']
       // const params = req.params
 
-      const event: WebhookEvent<unknown> = req.body
+      const event: WebhookEvent<StripeWebhookEventObject> = req.body
       switch (event.type) {
         case 'checkout.session.async_payment_failed':
+          logger.info(`Payment failed for session ${event.data.object.id}`)
           return
 
         case 'checkout.session.completed':
+          logger.info(`Payment completed for session ${event.data.object.id}`)
           return
 
         case 'checkout.session.async_payment_succeeded':
+          logger.info(`Payment succeeded for session ${event.data.object.id}`)
           return
 
         case 'checkout.session.expired':
+          logger.info(`Session expired for session ${event.data.object.id}`)
           return
 
         default:
