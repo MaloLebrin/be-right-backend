2c8a6b7853c2794c6ed688fcaa86f937643574c4
MaloLebrin <malolebrin@gmail.com>
2024-09-23 10:05:17 +0200

    Refactor StripeWebhookController to remove unused variables

--- src/controllers/stripe/StripeWebhookController.ts
+++ src/controllers/stripe/StripeWebhookController.ts
@@ -10,14 +10,14 @@ export class StripeWebhookController {
 
   constructor(DATA_SOURCE: DataSource) {
     if (DATA_SOURCE) {
-      this.StripeWebhookService = new StripeWebhookService(DATA_SOURCE)
+      this.StripeWebhookService = new StripeWebhookService()
     }
   }
 
   async events(req: Request, res: Response, next: NextFunction) {
     await wrapperRequest(req, res, next, async () => {
-      const sig = req.headers['stripe-signature']
-      const params = req.params
+      // const sig = req.headers['stripe-signature']
+      // const params = req.params
 
       const event: WebhookEvent<unknown> = req.body
       switch (event.type) {
@@ -37,7 +37,7 @@ export class StripeWebhookController {
           logger.info(`Unhandled event type ${event.type}`)
           break
       }
-      console.log({ event, params, sig }, '<==== {body, params}')
+
       return res.status(200).json({ message: 'ok' })
     })
   }
