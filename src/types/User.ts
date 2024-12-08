e4e830ae624e38179d35c43add114bbfdb01f965
MaloLebrin <malolebrin@gmail.com>
2024-12-08 17:31:42 +0100

    🏷️  Add ThemeEnum and CreateSettingPayload types; refactor User types

--- src/types/User.ts
+++ src/types/User.ts
@@ -1,13 +1,6 @@
 import type { Role } from './Role'
 import type { SubscriptionEnum } from './Subscription'
 
-export enum ThemeEnum {
-  LIGHT = 'light',
-  DARK = 'dark',
-}
-
-export const ThemeEnumArray = Object.values(ThemeEnum)
-
 export interface JWTTokenPayload {
   email: string
   firstName: string
