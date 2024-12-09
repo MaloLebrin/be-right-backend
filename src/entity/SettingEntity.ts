51f5955bf14ab62c2b042eecd508b98f84fd6667
MaloLebrin <malolebrin@gmail.com>
2024-12-09 08:44:33 +0100

    ✨ Add SettingEntity with paginatedRequestLimit and theme properties; update entity index

---
+++ src/entity/SettingEntity.ts
@@ -0,0 +1,23 @@
+import { Column, Entity, JoinColumn, OneToOne, RelationId } from 'typeorm'
+import { ThemeEnum } from '../types'
+import { BaseEntity } from './bases/BaseEntity'
+import { UserEntity } from './UserEntity'
+
+@Entity()
+export class SettingEntity extends BaseEntity {
+  @Column({ nullable: true, type: 'json', default: null })
+  paginatedRequestLimit: {
+    events: string
+    recipients: null
+  }
+
+  @Column({ type: 'enum', enum: ThemeEnum, default: ThemeEnum.LIGHT })
+  theme: ThemeEnum
+
+  @OneToOne(() => UserEntity)
+  @JoinColumn()
+  user: UserEntity
+
+  @RelationId((user: UserEntity) => user.id)
+  readonly userId: number
+}
