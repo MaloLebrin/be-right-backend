48068a2c1151c67a905fe270bd60da1ee60d3827
MaloLebrin <malolebrin@gmail.com>
2024-12-23 23:20:21 +0100

    ♻️  refactor SubscriptionService to handle subscription expiration and add createSubscription method in StripeSubscriptionService

--- src/services/stripe/stripeSubscription.service.ts
+++ src/services/stripe/stripeSubscription.service.ts
@@ -4,4 +4,14 @@ export class StripeSubscriptionService extends StripeService {
   constructor() {
     super()
   }
+
+  async createSubscription({ customerId }: { customerId: string }) {
+    if (!customerId) {
+      throw new Error('Customer id is required')
+    }
+
+    return this.stripe.subscriptions.create({
+      customer: customerId,
+    })
+  }
 }
