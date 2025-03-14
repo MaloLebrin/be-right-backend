a269a2439ea28efa681540b13cdf38f7227daf89
MaloLebrin <malolebrin@gmail.com>
2025-03-14 16:20:28 +0100

    ✨ Add file upload utility for Cloudinary integration

---
+++ src/utils/fileHelper.ts
@@ -0,0 +1,23 @@
+import cloudinary from 'cloudinary'
+import { CompanyEntity } from "../entity/Company.entity"
+import { useEnv } from "../env"
+import { FileTypeEnum } from "../types/File"
+
+const { NODE_ENV } = useEnv()
+
+export async function uploadFileToProvider({
+  file,
+  company,
+  typeOfFile,
+}: {
+  file: Express.Multer.File
+  company: CompanyEntity
+  typeOfFile: FileTypeEnum
+}) {
+  return cloudinary.v2.uploader.upload(file.path, {
+    folder: `beright-${NODE_ENV}/company-${company.name}-${company.id}/${typeOfFile}`,
+    quality: 'auto',
+    fetch_format: 'auto',
+    format: 'webp',
+  })
+}
