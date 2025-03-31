2d55a249e180cfb42e2bfb22ae41410a908c0c35
Malo <malolebrin@gmail.com>
2025-03-31 09:12:11 +0200

    🐛 FIX handle non-existent user case in NotificationSSEService and add… (#747)
    * 🐛 FIX handle non-existent user case in NotificationSSEService and add RegisterController stub
    
    * Gitbutler/workspace (#748)
    
    * GitButler Workspace Commit
    
    This is a merge commit the virtual branches in your workspace.
    
    Due to GitButler managing multiple virtual branches, you cannot switch back and
    forth between git branches and virtual branches easily. 
    
    If you switch to another branch, GitButler will need to be reinitialized.
    If you commit on this branch, GitButler will throw it away.
    
    Here are the branches that are currently applied:
     - main (refs/gitbutler/main)
       branch head: 9e2c3dd6e6f028be2d780b25c6e6af6c7c92d8ce
       - src/controllers/Auth/RegisterController.ts
    For more information about what we're doing here, check out our docs:
    https://docs.gitbutler.com/features/virtual-branches/integration-branch
    
    * 🔧 Refactor NotificationSSEService to use instance method for Redis cache access
    
    ---------
    
    Co-authored-by: GitButler <gitbutler@gitbutler.com>
    
    ---------
    
    Co-authored-by: GitButler <gitbutler@gitbutler.com>

--- src/services/notifications/notificationSSEService.ts
+++ src/services/notifications/notificationSSEService.ts
@@ -1,6 +1,5 @@
 import { type DataSource, IsNull, type Repository } from 'typeorm'
 import { UserEntity } from '../../entity/UserEntity'
-import { ApiError } from '../../middlewares/ApiError'
 import { NotificationSubcriptionEntity } from '../../entity/notifications/NotificationSubscription.entity'
 import type { NotificationEntity } from '../../entity/notifications/Notification.entity'
 import { REDIS_CACHE } from '../../'
@@ -22,7 +21,7 @@ export class NotificationSSEService {
   }: {
     notificationToken: string
   }): Promise<NotificationEntity[]> => {
-    const user = await REDIS_CACHE.get<{ id: number; notificationToken: string }>(
+    const user = await this.redisCache.get<{ id: number; notificationToken: string }>(
       `user-notification-${notificationToken}`,
       () => this.UserRepository.findOne({
         where: {
@@ -36,7 +35,7 @@ export class NotificationSSEService {
     )
 
     if (!user) {
-      throw new ApiError(422, 'L\'utilisateur n\'existe pas')
+      return []
     }
 
     const notificationSubscriptions = await this.NotificationSubscriptionRepository.find({
